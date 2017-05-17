46774a7ecace89302827f1b0758f682ba2189808
Mohammadsa <mohammadsa78@yahoo.com>
2017-05-17 17:16:41 +0430

    Rebrand the project

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Custom Kernel build script
-
+# Special thanks to GuneetAtwal for his great build script and kernel for kiwi
 # Constants
 green='\033[01;32m'
 red='\033[01;31m'
@@ -12,41 +12,41 @@ blue='\033[0;34m'
 default='\033[0m'
 
 # Resources
-ANDROID_DIR=/home/guneetatwal/android
+ANDROID_DIR=~/android
 KERNEL_DIR=$PWD
 IMAGE=$KERNEL_DIR/arch/arm64/boot/Image
 #IMAGE=$KERNEL_DIR/arch/arm/boot/zImage for 32 bit architecture
 DTBTOOL=$KERNEL_DIR/scripts/dtbTool
-TOOLCHAIN=$ANDROID_DIR/toolchain/aarch64-linux-android-4.9-aosp/bin
+TOOLCHAIN=$ANDROID_DIR/linaro6/bin
 
 #Paths
 OUT_DIR=$KERNEL_DIR/out
-OUT_ZIP=$KERNEL_DIR/Blaze-Releases
+OUT_ZIP=$KERNEL_DIR/Releases
 NEW_OUT=$OUT_DIR/tools
 
 # Kernel Version Info
-BASE="-Blaze™Kernel"
-CUR_VER=""
-BLAZE_VER="$BASE$CUR_VER"
+BASE="-HotDoG™"
+CUR_VER="-x"
+HD_VER="$BASE$CUR_VER"
  
 
 # Variables
 
-DEFCONFIG="blaze_defconfig"
-export LOCALVERSION=~`echo $BLAZE_VER`
+DEFCONFIG="hd_defconfig"
+export LOCALVERSION=~`echo $HD_VER`
 export CROSS_COMPILE=$TOOLCHAIN/aarch64-linux-android-
 export ARCH=arm64
-export KBUILD_BUILD_USER="GuneetAtwal"
-export KBUILD_BUILD_HOST="BeastPC"
+export KBUILD_BUILD_USER="mohammadsa9"
+export KBUILD_BUILD_HOST="kiwi"
 
-function make_blaze {
+function make_hd {
 		echo -e "$green*******************************************************"
-		echo "                  Compiling $BLAZE_VER	              "
+		echo "                  Compiling $HD_VER	              "
 		echo -e "*****************************************************"
 		echo
 		make $DEFCONFIG
 		make menuconfig
-		make -j5
+		make -j9
 		rm -rf $NEWOUT/Image
 		cp -vr $IMAGE $NEW_OUT
 		make_dtb
@@ -62,18 +62,17 @@ function make_clean {
 		echo "          Cleaning up object files and other stuff	              "
 		echo -e "***********************************************$default"
 		make mrproper
-		make_blaze
+		make_hd
 	}
 		
 function make_recompile {
 			echo -e "$cyan*******************************************************"
-			echo "             Recompiling $BLAZE_VER	              "
+			echo "             Recompiling $HD_VER	              "
 			echo -e "*****************************************************"
-			make -j5
+			make -j8
 			rm -rf $NEWOUT/Image
 			cp -vr $IMAGE $NEW_OUT
 			make_dtb
-			strip_modules
 			make_zip
 			housekeeping
 		}
@@ -83,7 +82,7 @@ function make_dtb {
 			echo "             		Creating dt.img....	              "
 			echo -e "*****************************************************"
 			rm -rf $NEWOUT/dt.img
-			$DTBTOOL -v -s 2048 -o $NEW_OUT/dt.img -p scripts/dtc/ arch/arm/boot/dts/
+			$DTBTOOL -v -s 2048 -o $NEW_OUT/dt.img -p scripts/dtc/ arch/arm64/boot/dts/
 		}
 
 function make_zip {
@@ -92,9 +91,9 @@ function make_zip {
 		echo -e "*****************************************************"
 		cd $OUT_DIR
 		rm -f '*.zip'
-		zip -yr BlazeKernel`echo $CUR_VER`.zip *
-		mv BlazeKernel`echo $CUR_VER`.zip $OUT_ZIP
-		echo "       Find your zip in Blaze-Releases directory"
+		zip -yr HDKernel`echo $CUR_VER`.zip *
+		mv HDKernel`echo $CUR_VER`.zip $OUT_ZIP
+		echo "       Find your zip in Releases directory"
 		echo -e "$default"
 		cd $KERNEL_DIR 
 		}
@@ -113,7 +112,7 @@ while read -p " 'Y' to Compile all , 'R' to clean and recompile , 'C' to to do a
 do
 case "$choice" in
 	y|Y)
-		make_blaze
+		make_hd
 		break
 		;;
 	r|R )
@@ -129,7 +128,7 @@ case "$choice" in
 		;;
 	* )
 		echo
-		echo "Are you drunk???"
+		echo "Wrong input"
 		echo
 		;;
 esac
@@ -137,3 +136,4 @@ done
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
 echo -e "$green Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$default"
+
