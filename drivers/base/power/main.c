2cef0201a6fc98c5b5f993c36ffed6b01cf00054
Mohammadsa9 <mohammadsa9@DESKTOP-P5955AA.localdomain>
2018-06-27 18:13:50 +0430

    boeffla_wl_blocker updated to v1.0.1 Some derps fixed + Enabled

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -592,6 +592,8 @@ static int device_resume_early(struct device *dev, pm_message_t state)
 	return error;
 }
 
+extern void print_active_wakeup_sources(void);
+
 /**
  * dpm_resume_early - Execute "early resume" callbacks for all devices.
  * @state: PM transition of the system being carried out.
@@ -599,6 +601,10 @@ static int device_resume_early(struct device *dev, pm_message_t state)
 static void dpm_resume_early(pm_message_t state)
 {
 	ktime_t starttime = ktime_get();
+	
+#ifdef CONFIG_BOEFFLA_WL_BLOCKER
+	print_active_wakeup_sources();
+#endif
 
 	mutex_lock(&dpm_list_mtx);
 	while (!list_empty(&dpm_late_early_list)) {
@@ -755,9 +761,6 @@ void dpm_resume(pm_message_t state)
 
 	might_sleep();
 
-#ifdef CONFIG_BOEFFLA_WL_BLOCKER
-	pm_print_active_wakeup_sources();
-#endif
 
 	mutex_lock(&dpm_list_mtx);
 	pm_transition = state;
--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -20,10 +20,12 @@
 
 
 #ifdef CONFIG_BOEFFLA_WL_BLOCKER
-char list_wl[255];
-char list_wl_search[257];
+#include "boeffla_wl_blocker.h"
+
+char list_wl_search[LENGTH_LIST_WL_SEARCH] = {0};
 bool wl_blocker_active = false;
 bool wl_blocker_debug = false;
+static void wakeup_source_deactivate(struct wakeup_source *ws);
 #endif
 
 /*
@@ -414,7 +416,7 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 // AP: Function to check if a wakelock is on the wakelock blocker list
 static bool check_for_block(struct wakeup_source *ws)
 {
-	char wakelock_name[52];
+	char wakelock_name[52] = {0};
 
 	// if debug mode on, print every wakelock requested
 	if (wl_blocker_debug)
@@ -424,25 +426,38 @@ static bool check_for_block(struct wakeup_source *ws)
 	if (!wl_blocker_active)
 		return false;
 
-	// check if wakelock is in wake lock list to be blocked
+	// only if ws structure is valid
 	if (ws)
 	{
 		// wake lock names which are longer than 50 chars are not handled
 		if (strlen(ws->name) > 50)
 			return false;
 
+		// check if wakelock is in wake lock list to be blocked
 		sprintf(wakelock_name, ";%s;", ws->name);
 
 		if(strstr(list_wl_search, wakelock_name) == NULL)
 			return false;
+
+		// wake lock is in list, print it if debug mode on
+		if (wl_blocker_debug)
+			printk("Boeffla WL blocker: %s blocked\n", ws->name);
+
+		// if it is currently active, deactivate it immediately + log in debug mode
+		if (ws->active)
+		{
+			wakeup_source_deactivate(ws);
+
+			if (wl_blocker_debug)
+				printk("Boeffla WL blocker: %s killed\n", ws->name);
+		}
+
+		// finally block it
+		return true;
 	}
 
-	// wake lock is in list, print it if debug mode on
-	if (wl_blocker_debug)
-		printk("Boeffla WL blocker: %s blocked\n", ws->name);
-
-	// finally block it
-	return true;
+	// there was no valid ws structure, do not block by default
+	return false;
 }
 #endif
 
@@ -718,7 +733,7 @@ void pm_wakeup_event(struct device *dev, unsigned int msec)
 }
 EXPORT_SYMBOL_GPL(pm_wakeup_event);
 
-static void print_active_wakeup_sources(void)
+void print_active_wakeup_sources(void)
 {
 	struct wakeup_source *ws;
 	int active = 0;
@@ -745,6 +760,8 @@ static void print_active_wakeup_sources(void)
 			last_activity_ws->name);
 	rcu_read_unlock();
 }
+
+EXPORT_SYMBOL_GPL(print_active_wakeup_sources);
 
 /**
  * pm_wakeup_pending - Check if power transition in progress should be aborted.
